1ea75f379e843ca08d33d1ca40cd9a3a9c4c5ea8
Mikaayel Rehman <mika@Mikaayels-iMac.local>
2017-10-21 13:00:27 +0100

    updated routes on serverjs

--- server.js
+++ server.js
@@ -1,24 +1,46 @@
-const Express = require('express');
-const Router = require('react-router');
+// server.js
 
-let app = Express();
-const PORT = process.env.PORT || 8080;
+// BASE SETUP
+// ==================================================================
+const Express 	= require('express');
+let app 		= Express();
+const PORT 		= process.env.PORT || 8080;
 
-/** forward http to https */
-// app.use(function(req, res, next) {
-// 	if(req.headers['x-forwarded-proto'] === 'https') {
-// 		next();
-// 	} else {
-// 		res.redirect('https://' + req.hostname + req.url);
-// 	}
-// });
+// HANDLE HTTPS REDIRECTION
+// ==================================================================
+if(PORT !== 8080) {
+	app.use(function(req, res, next) {
+		if(req.headers['x-forwarded-proto'] === 'https') {
+			next();
+		} else {
+			res.redirect('https://' + req.hostname + req.url);
+		}
+	});
+}
 
-app.get('/*', (req, res) => {
-	res.render('index');
+// ROUTES
+// ==================================================================
+app.get('/', (req, res) => {
+	res.send('<p>home page</p>');
+	console.log(req.method, req.url);
 });
 
-// app.use(Express.static('dist'));
+app.get('/about', (req, res) => {
+	res.send('<p>about page</p>');
+	console.log(req.method, req.url);
+});
 
-app.listen(PORT, function() {
-	console.log(`Express server is running on port ${PORT}`);
-})
+app.get('/catalogue', (req, res) => {
+	res.send('<p>catalogue page</p>');
+	console.log(req.method, req.url);
+});
+
+app.get('/catalogue/:name', (req, res) => {
+	res.send(`<p>catalogue page: Item: ${req.params.name}</p>`);
+	console.log(req.method, req.url, req.params);
+});
+
+// START SERVER
+// ==================================================================
+app.listen(PORT);
+console.log('server is running on port:', PORT);
