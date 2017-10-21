cba7963639ebf820908c456dbe22618de0b9c642
Mikaayel Rehman <mika@Mikaayels-iMac.local>
2017-10-21 09:25:38 +0100

    removed firebase config, updated pro start script name, updated server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 const Express = require('express');
 
 let app = Express();
+const PORT = process.env.PORT || 8080;
+
+/** forward http to https */
+// app.use(function(req, res, next) {
+// 	if(req.headers['x-forwarded-proto'] === 'https') {
+// 		next();
+// 	} else {
+// 		res.redirect('https://' + req.hostname + req.url);
+// 	}
+// })
 
 app.use(Express.static('dist'));
 
-app.listen(8080, function() {
-	console.log('server is running on port: 8080');
-});
+app.listen(Port, function() {
+	console.log(`Express server is running on port ${PORT}`);
+})
